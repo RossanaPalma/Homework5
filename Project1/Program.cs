ceff932a78c52c87b72f4bc64228c65bcbe290f3
RossanaPalma <70126235+RossanaPalma@users.noreply.github.com>
2020-10-05 17:09:06 -0700

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks.Sources;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] name = new string[10] { "Sophia", "Emilly", "Emma", "Grace", "Hanna", "John", "Steve", "William", "Jacob", "Robert" };
+            int i;
+            Console.WriteLine(name.Length);
+            for (i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine("Enter with the name  " + (i));
+                Console.WriteLine(name[i]);
+            }
+
+            double[] score = new double[10]; //{ "8.5", "6.2", "7.0", "9.1", "5.7", "7.3", "7.5", "5.5", "6.8", "8.2" };
+            int index;
+            Console.WriteLine(score.Length);
+            for (index = 0; index < score.Length; index++)
+            {
+                Console.WriteLine("Enter with score  " + (index));
+                score[index] = double.Parse(Console.ReadLine()); 
+            }
+
+
+
+        }
+    }
+}
---
+++ Project2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] newNum = numbers;
+            Console.WriteLine("The original array is : ");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i + " ");
+            }
+            Array.Reverse(numbers);
+            Console.WriteLine("The reverse array is : ");
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+
+            //int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Array.Reverse(numbers);
+            //Console.WriteLine("The reverse array is: " + Convert.ToInt32(numbers));
+
+
+
+
+
+
+
+
+        }
+    }
+}
